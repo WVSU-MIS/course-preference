ede907b8424889bf6a8cd66a729c02531a545d17
unknown <louiecervantes@wvsu.edu.ph>
2023-05-20 06:59:52 +0800

    save changes

--- .ipynb_checkpoints/streamlit_app-checkpoint.py
+++ .ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -120,6 +120,26 @@ def app():
     st.write('Distribution of First Priority and Province')
     twoway_plot(df, 'First Priority', 'Province')
     
+    st.write('Natural Language Processing (NLP) methods were applied in the analysis of course preference data to extract insights from textual information provided by participants. Here are the NLP methods used in analyzing course preference data:')
+
+    st.write('Text Preprocessing: Before applying NLP techniques, it\'s essential to preprocess the textual data. This typically involves tasks such as removing punctuation, converting text to lowercase, removing stopwords (common words like "the," "and," etc.), and performing stemming or lemmatization to reduce words to their root forms. This preprocessing step helps to standardize the text and improve the accuracy of subsequent NLP analyses.')
+
+    st.write('Sentiment Analysis: Sentiment analysis, also known as opinion mining, is used to determine the sentiment expressed in the text. In the context of course preference analysis, sentiment analysis can be used to gauge participants\' attitudes or emotions towards different courses. This analysis can be performed by using machine learning algorithms or predefined sentiment lexicons to classify text as positive, negative, or neutral.')
+
+    st.write('POS tagging and chunking were methods used in the course preference analysis to extract relevant information from textual data related to course preferences. Here\'s an overview of how these techniques were applied:')
+
+    st.write('POS Tagging:\n \
+POS tagging assigns part-of-speech tags to each word in a sentence or text. In the context of course preference analysis, POS tagging can help identify the grammatical roles and categories of words, allowing for a better understanding of the text. By applying POS tagging, you can extract relevant information such as course names, subjects, or academic disciplines mentioned in the text.')
+
+    st.write('For example, if a sentence mentions "I am interested in teaching english teaching courses," POS tagging would label "I" as a pronoun, "am" as a verb, "interested" as an adjective, "in" as a preposition, "teaching english" as a noun phrase, and "courses" as a noun. This tagging can help identify the subject of interest (teaching english) and the type of courses mentioned.')
+
+    st.write('Chunking:\n \
+Chunking, also known as shallow parsing, involves grouping words into meaningful phrases or chunks based on their grammatical structure. It helps extract higher-level syntactic units from the text. In the context of course preference analysis, chunking can be used to identify and extract phrases or noun phrases that provide insights into participants\' preferences.')
+    
+    st.write('For example, consider the sentence: "I prefer advanced mathematics and physics courses." Chunking would identify and extract the noun phrases "advanced mathematics" and "physics courses." These chunks can provide information about the specific subjects or academic areas that the participant prefers.')
+
+    st.write('By combining POS tagging and chunking, we can extract relevant phrases or noun phrases related to course preferences. These extracted chunks can be further analyzed to identify popular subjects, specific course preferences, or academic disciplines that are frequently mentioned by participants.')
+    
 #run the app
 if __name__ == "__main__":
     app()
