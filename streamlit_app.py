648db9772fc44821b7ee5675f06094e5654e3517
unknown <louiecervantes@wvsu.edu.ph>
2023-05-20 05:37:53 +0800

    save changes

---
+++ streamlit_app.py
@@ -0,0 +1,88 @@
+#Input the relevant libraries
+import numpy as np
+import pandas as pd
+import streamlit as st
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+
+def loadcsvfile():
+    csvfile = 'coe-course-prefs.csv'
+    df = pd.read_csv(csvfile, dtype='str', header=0, sep = ",", encoding='latin') 
+    return df
+
+def createPlots(df, columnName):
+    st.write('Distribution by ' + columnName)
+    scounts=df[columnName].value_counts()
+    labels = list(scounts.index)
+    sizes = list(scounts.values)
+    fig = plt.figure(figsize=(12, 4))
+    plt.subplot(1, 2, 1)
+    plt.pie(sizes, labels = labels, textprops={'fontsize': 10}, startangle=140, \
+            autopct='%1.0f%%', colors=sns.color_palette('Set2'))
+    plt.subplot(1, 2, 2)
+    p = sns.barplot(x = scounts.index, y = scounts.values, palette= 'viridis')
+    plt.setp(p.get_xticklabels(), rotation=90)
+    st.pyplot(fig)
+
+    # get value counts and percentages of unique values in column 
+    value_counts = df[columnName].value_counts(normalize=True)
+    value_counts = value_counts.mul(100).round(2).astype(str) + '%'
+    value_counts.name = 'Percentage'
+
+    # combine counts and percentages into a dataframe
+    result = pd.concat([df[columnName].value_counts(), value_counts], axis=1)
+    result.columns = ['Counts', 'Percentage']
+    st.write(pd.DataFrame(result))
+    
+    return
+
+def createTable(df, columnName):  
+    st.write('Graduate Distribution by ' + columnName)
+    # get value counts and percentages of unique values in column 
+    value_counts = df[columnName].value_counts(normalize=True)
+    value_counts = value_counts.mul(100).round(2).astype(str) + '%'
+    value_counts.name = 'Percentage'
+
+    # combine counts and percentages into a dataframe
+    result = pd.concat([df[columnName].value_counts(), value_counts], axis=1)
+    result.columns = ['Counts', 'Percentage']
+    st.write(pd.DataFrame(result))
+    
+    return
+
+def twowayPlot(df, var1, var2):
+    fig = plt.figure(figsize =(10, 3))
+    p = sns.countplot(x=var1, data = df, hue=var2, palette='bright')
+    _ = plt.setp(p.get_xticklabels(), rotation=90) 
+    st.pyplot(fig)
+
+# Define the Streamlit app
+def app():
+    st.title("2023 WVSU College of Education Course Preference Survey")      
+                 
+    st.write("The qualifiers for the interview among the incoming first year students of the College \
+    of Education were asked to participate in this course-preference survey. There were 207 respondents \
+    who filled up the survey form.")
+                 
+    st.write("A course preference study is a research study conducted to examine the preferences \
+    and interests of individuals regarding their choice of courses or subjects to pursue in an \
+    educational setting. It aims to gather data on the factors that influence students' \
+    decision-making process when selecting courses and to gain insights into their motivations \
+    and preferences..")
+
+    df = loadcsvfile()
+    st.write('Distribution by Sex')
+    createPlots(df, 'Sex')
+    
+    st.write('Distribution by Civil Status')
+    createPlots(df, 'Civil Status')
+
+    st.write('Distribution By First Priority Course'):
+    createPlots(df, 'First Priority')
+    
+
+#run the app
+if __name__ == "__main__":
+    app()
